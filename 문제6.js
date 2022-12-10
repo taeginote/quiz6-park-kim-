f1ee6a695804837f936a41f8331743c0dac9a38b
taeginote <ab315601@naver.com>
2022-12-11 00:11:58 +0900

    quiz6
    playlist, newplaylist 추가

---
+++ 문제6.js
@@ -0,0 +1,60 @@
+// 플레이 리스트 안에 음악은 최대 5개
+// 신곡 5개
+
+let playlist = {
+    music1: {
+        title: "After LIKE",
+        singer: "IVE",
+        date: "2022.08.22",
+    },
+    music2: {
+        title: "자격지심",
+        singer: "비오(Feat.ZICO)",
+        date: "2022.09.29",
+    },
+    music3: {
+        title: "사건의 지평선",
+        singer: "윤하",
+        date: "2022.03.30",
+    },
+    music4: {
+        title: "WHEN I MOVE",
+        singer: "카라",
+        date: "2022.11.29",
+    },
+    music5: {
+        title: "나의 X에게",
+        singer: "경서",
+        date: "2022.04.24",
+    },
+};
+console.log(playlist);
+
+let newplaylist = {
+    music1: {
+        title: "London Boy",
+        singer: "임영웅",
+        date: "2022.11.15",
+    },
+    music2: {
+        title: "잘가요",
+        singer: "주호",
+        date: "2022.12.04",
+    },
+    music3: {
+        title: "모든 계절은 너였다",
+        singer: "길구봉구",
+        date: "2022.11.13",
+    },
+    music4: {
+        title: "찬바람이 불어오네요",
+        singer: "케이시",
+        date: "2022.11.29",
+    },
+    music5: {
+        title: "나쁜X",
+        singer: "비비",
+        date: "2022.11.18",
+    },
+};
+console.log(newplaylist.music5);
