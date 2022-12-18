d9e8320a28a1e8ab2f720e85e83ac69c4c0343b6
seonyeong719 <sy904280@gmail.com>
2022-12-18 23:39:06 +0900

    Rename: 파일 추가
    퀴즈6번 문제 만들기 과제

--- 문제6.js
+++ 문제6.js
@@ -2,14 +2,15 @@
 뽑기는 300원, 500원 두가지가 있다. 
 300원은 상품 1개, 500원은 상품 2개가 랜덤으로 나온다.
 상품값 보다 더 많은 코인을 넣으면 잔돈이 반환된다.
+그리고 상품값 보다 적은 돈을 넣으면 잔액이 부족 하다고 나온다.
 
-1. 아이폰, 아이패드, 에어팟, 노트북, 워치 상품 5개의 객체 선언.
-2. ..?
+함수명은 game이고, 파라미터로 coin, choice를 받는다.
 */
 
 function changes(coin, choice) {
   return coin - choice;
 }
+
 function game(coin, choice) {
   let item = {
     1: "iphone",
@@ -28,6 +29,6 @@ function game(coin, choice) {
   if (coin >= choice && choice === 300) return console.log(`잔돈 : ${change}, 상품 : ${a}`);
   if (coin >= choice && choice === 500)
     return console.log(`잔돈 : ${change}, 상품1 : ${a}, 상품2 : ${b}`);
-  if (coin < 300) return console.log("돈없어 임마");
+  if (coin < 500) return console.log("잔액이 부족합니다");
 }
-game(1500, 500);
+game(600, 500);
